c06d94ee9dff5fbad70203b4b810adc71e364a79
Gustavo Prezoto <gustavoprezotoboca@gmail.com>
2025-11-02 18:03:57 -0300

    Renamed database scripts folder.

---
+++ database_scripts/init.sql
@@ -0,0 +1,4 @@
+INSERT INTO users (email, name, password_hash, role) VALUES
+('admin@ellp.com', 'admin', '$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjPGga31lW', 'admin'),
+('professor@ellp.com', 'exemplo professor', '$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjPGga31lW', 'professor'),
+('aluno@ellp.com', 'exemplo aluno', '$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjPGga31lW', 'aluno');
